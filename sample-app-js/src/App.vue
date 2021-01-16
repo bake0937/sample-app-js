4a73ccf38d8ddce973a7721898b2087623223eb0
bake0937 <sadayasu.info@gmail.com>
2021-01-16 16:17:55 +0900

    composition-apiを利用できるようにする

--- sample-app-js/src/App.vue
+++ sample-app-js/src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
+
+<script>
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({});
+</script>
 
 <style lang="scss">
 #app {
